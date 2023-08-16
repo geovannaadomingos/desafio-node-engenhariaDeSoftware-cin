c8b56aea740cfa080899d19dc5efb0a0ebb23b10
Geovanna Domingos <gmdomingosn@gmail.com>
2023-08-16 13:11:20 -0300

    adiciona teste par ao put do server.js no server.test.js

--- tests/server.test.js
+++ tests/server.test.js
@@ -12,6 +12,7 @@ let series = [
 ]
 
 describe('Testando rotas do Server', () => {
+    // testando o get
     it('Deve retornar a lista completa de filmes', async () => {
         const response = await request(server).get('/movies')
         expect(response.status).toBe(200)
@@ -48,6 +49,7 @@ describe('Testando rotas do Server', () => {
         expect(response.body.message).toBe('Conteúdo não encontrado.')
     })
 
+    // testando o post
     it('Deve adicionar um novo filme', async () => {
         const newMovie = { title: 'Midnight in Paris', director: ' Woody Allen' }
         const response = await request(server).post('/movies').send(newMovie)
@@ -77,6 +79,53 @@ describe('Testando rotas do Server', () => {
         const response = await request(server).post('/série').send({ title: 'Manifest: O Mistério do Voo 828', director: 'Jeff Rake', seasons: 4 })
         expect(response.status).toBe(404)
         expect(response.body.message).toBe('Não foi possível adicionar conteúdo.')
+    })
+
+    // testando o put
+    it('Deve atualizar um filme existente com sucesso', async () => {
+        const updatedMovie = { title: 'Midnight in Paris' }
+        const response = await request(server).put('/movies/1').send(updatedMovie)
+        expect(response.status).toBe(200)
+        expect(response.body.title).toBe(updatedMovie.title)
+        expect(response.body.director).toBe(updatedMovie.director)
+
+        const movieResponse = await request(server).get('/movies/1')
+        expect(movieResponse.status).toBe(200)
+        expect(movieResponse.body).toEqual(response.body)
+    })
+
+    it('Deve retornar status 404 para atualização de filme inexistente', async () => {
+        const updatedMovie = { title: 'Midnight in Paris' }
+        const response = await request(server).put('/movies/999').send(updatedMovie)
+        expect(response.status).toBe(404)
+        expect(response.body.message).toBe('Filme não encontrado.')
+    })
+
+    it('Deve atualizar uma série existente com sucesso', async () => {
+        const updatedSeries = { title: 'Manifest: O Mistério do Voo 828', director: 'Jeff Rake', seasons: 5 }
+        const response = await request(server).put('/series/1').send(updatedSeries)
+        expect(response.status).toBe(200)
+        expect(response.body.title).toBe(updatedSeries.title)
+        expect(response.body.director).toBe(updatedSeries.director)
+        expect(response.body.seasons).toBe(updatedSeries.seasons)
+
+        const seriesResponse = await request(server).get('/series/1')
+        expect(seriesResponse.status).toBe(200)
+        expect(seriesResponse.body).toEqual(response.body)
+    })
+
+    it('Deve retornar status 404 para atualização de série inexistente', async () => {
+        const updatedSeries = { title: 'Manifest: O Mistério do Voo 828', director: 'Jeff Rake', seasons: 5 }
+        const response = await request(server).put('/series/999').send(updatedSeries)
+        expect(response.status).toBe(404)
+        expect(response.body.message).toBe('Série não encontrado.')
+    })
+
+    it('Deve retornar status 404 para atualização com mediaType inválido', async () => {
+        const updatedMedia = { title: 'Manifest: O Mistério do Voo 828' }
+        const response = await request(server).put('/séries/1').send(updatedMedia)
+        expect(response.status).toBe(404)
+        expect(response.body.message).toBe('Não foi possível alterar o conteúdo.')
     })
 
 })
